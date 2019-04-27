a00f0b31b3faf65be2b6baec573be1be636db402
Morten D. Hansen <morten@visia.dk>
2019-04-27 11:29:57 +0200

    Init project

---
+++ tests/BaseTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MortenDHansen\EloquentDocblockGenerator\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class baseTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function base_test()
+    {
+        $this->assertTrue(true);
+    }
+}
